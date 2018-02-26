3477821e271cb0ad683c63e54293e686ab4a6690
RyumaRyama <homes.ryu@gmail.com>
2018-02-27 02:41:38 +0900

    Edit rb file.

--- ptob.rb
+++ ptob.rb
@@ -8,21 +8,23 @@ class Ptob < Formula
 	url "https://github.com/RyumaRyama/PDFtoBook", :tag => "v#{HOMEBREW_BREWALL_VERSION}"
 	version HOMEBREW_BREWALL_VERSION
 	sha256 ""
+	head 'https://github.com/RyumaRyama/homebrew-ptob.git', :branch => 'master'	
+
 	skip_clean 'bin'
 	# depends_on "cmake" => :build
 	def install
 		# ENV.deparallelize  # if your formula fails when building in parallel
 		# Remove unrecognized options if warned by configure
-		system "./configure", "--disable-debug",
-							  "--disable-dependency-tracking",
-							  "--disable-silent-rules",
-							  "--prefix=#{prefix}"
+		#system "./configure", "--disable-debug",
+		#					  "--disable-dependency-tracking",
+		#					  "--disable-silent-rules",
+		#					  "--prefix=#{prefix}"
 		# system "cmake", ".", *std_cmake_args
 		system "make", "install" # if this fails, try separate make/make install steps
 		prefix.install 'bin'
 		(bin+'brewall').chmod 0755
 	end
-	test do
+	#test do
 		# `test do` will create, run in and delete a temporary directory.
 		#
 		# This test will fail and we won't accept that! For Homebrew/homebrew-core
@@ -32,6 +34,6 @@ class Ptob < Formula
 		#
 		# The installed folder is not in the path, so use the entire path to any
 		# executables being tested: `system "#{bin}/program", "do", "something"`.
-		system "false"
-	end
+		#system "false"
+	#end
 end
